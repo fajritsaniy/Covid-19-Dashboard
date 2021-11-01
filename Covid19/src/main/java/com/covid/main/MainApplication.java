3d1a7305b5178221b91f01fa56a94a392c1b9ad5
Fajri <Fajri@DESKTOP-64DSE3R>
2021-11-01 10:33:16 +0700

    Update JPA

--- Covid19/src/main/java/com/covid/main/MainApplication.java
+++ Covid19/src/main/java/com/covid/main/MainApplication.java
@@ -12,7 +12,9 @@ public class MainApplication {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				SpringApplication.run(MainApplication.class, args);
-			}
+				
+				
+	}
 
 		}
 
